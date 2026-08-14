d70fedbbae114523191828570ab121e91c65f2cb
Tom <galois0413@gmail.com>
2026-08-14 15:06:46 +0900

    feat: add class 3 analysis data product

--- data_pipeline/aggregates/__init__.py
+++ data_pipeline/aggregates/__init__.py
@@ -5,9 +5,11 @@ from .company_counterparty_product_month import (
     aggregate_company_counterparty_product_month,
     validate_forward_supply_rows,
 )
+from .class3_analysis import build_class3_analysis
 
 __all__ = [
     "UnsupportedTransactionTypeError",
     "aggregate_company_counterparty_product_month",
+    "build_class3_analysis",
     "validate_forward_supply_rows",
 ]
